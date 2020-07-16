c0cc07de62394102b8a49af810368d2cdec63a92
Nick Chen <chenyahua2012@hotmail.com>
2020-07-16 16:51:59 -0400

    connect vue to shopify theme

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="how">
+        <!-- <Header-temp /> -->
+            {{msg}}
+            <!-- <router-view></router-view> -->
+        <!-- <Footer-temp /> -->
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "App",
+        data: function() {
+            return {
+                msg: 'helloljfbajksbdjc'
+            }
+        }
+    }
+</script>
