6c155169735192d32f6106f9754c47bdbf365dad
Nick Chen <chenyahua2012@hotmail.com>
2020-07-18 13:59:16 -0400

    display products in a grid table

---
+++ src/components/Slider.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="slider-outter-box">
+        {{ msg }}
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "Slider",
+        data: ()=>{
+            return (
+                {msg: 'hello vue'}
+            )
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
   
 import Vue from "vue";
-import App from "./App.vue";
-// import router from "./router";
+import Slider from "./components/Slider.vue";
 
 new Vue({
-    // router,
-    render: h => h(App)
-}).$mount("#app");
+    render: h => h(Slider)
+}).$mount("#slide-show");
